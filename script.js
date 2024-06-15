304efb672aff402d6700d91798ce78d04c2360e0
YashBachwani <yash.bachwani30@gmail.com>
2024-06-15 19:55:34 +0530

    New Changes

---
+++ script.js
@@ -0,0 +1,27 @@
+let name = prompt("Enter your name");
+let news1 = document.getElementById("hours");
+let e = new Date().getHours();
+let r = new Date();
+let greetings;
+let timing = document.getElementById("current-time");
+let time
+if(e>=0 && e<4){
+    greetings="Night Is still Young/Midnight ";
+    time = "Your time is "+r.toLocaleTimeString();
+}
+else if(e>=4 && e<12){
+    greetings="Good Morrning ";
+    time = "Your time is "+r.toLocaleTimeString();
+}
+else if(e>=12 && e<17){
+    greetings="Good Afternoon ";
+    time = "Your time is "+r.toLocaleTimeString();}
+else if(e>=17 && e<20){
+    greetings="Good Evening ";
+    time = "Your time is "+r.toLocaleTimeString();}
+else{
+    greetings="Good Night ";
+    time = "Your time is "+r.toLocaleTimeString();
+}
+    news1.innerHTML = greetings + name ;
+    timing.innerHTML = time;
